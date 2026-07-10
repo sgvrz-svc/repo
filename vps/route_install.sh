9789d48fd676b172c9e5bdaaa73e57419db499d0
sgvrz-svc <294488015+sgvrz-svc@users.noreply.github.com>
2026-07-10 19:23:57 +0300

    upd

--- vps/route_install.sh
+++ vps/route_install.sh
@@ -5,7 +5,6 @@ WG_IF="wg0"
 AMZ_IF="awg0"
 TABLE="100"
 MARK="0x1"
-
 EXT_IF=$(ip route show default | awk '/default/ {print $5; exit}')
 
 apt update
@@ -16,26 +15,6 @@ chmod 600 /etc/wireguard/wg0.conf
 
 sysctl -w net.ipv4.ip_forward=1
 echo "net.ipv4.ip_forward=1" > /etc/sysctl.d/99-forward.conf
-
-cat > /etc/nftables.conf <<EOF
-flush ruleset
-
-table inet filter {
-  chain forward {
-    type filter hook forward priority 0; policy accept;
-    iif "$AMZ_IF" oif "$WG_IF" accept
-    iif "$WG_IF" oif "$AMZ_IF" ct state established,related accept
-  }
-}
-
-table ip nat {
-  chain postrouting {
-    type nat hook postrouting priority 100; policy accept;
-    oif "$WG_IF" masquerade
-    oif "$EXT_IF" masquerade
-  }
-}
-EOF
 
 cat > /etc/systemd/system/wg-policy.service <<EOF
 [Unit]
@@ -53,8 +32,41 @@ RemainAfterExit=yes
 WantedBy=multi-user.target
 EOF
 
+cat > /etc/systemd/system/nftables-wg.service <<EOF
+[Unit]
+After=wg-quick@wg0.service wg-policy.service
+Requires=wg-quick@wg0.service wg-policy.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/sbin/nft -f /etc/nftables.conf
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > /etc/nftables.conf <<EOF
+flush ruleset
+
+table inet filter {
+  chain forward {
+    type filter hook forward priority 0; policy accept;
+    iifname "$AMZ_IF" oifname "$WG_IF" accept
+    iifname "$WG_IF" oifname "$AMZ_IF" ct state established,related accept
+  }
+}
+
+table ip nat {
+  chain postrouting {
+    type nat hook postrouting priority 100; policy accept;
+    oifname "$WG_IF" masquerade
+    oifname "$EXT_IF" masquerade
+  }
+}
+EOF
+
 systemctl daemon-reload
-systemctl enable nftables
-systemctl restart nftables
 systemctl enable --now wg-quick@wg0
 systemctl enable --now wg-policy.service
+systemctl enable --now nftables-wg.service
