9f8edf506da85206cd51185000252d63cf2e32d9
sgvrz-svc <294488015+sgvrz-svc@users.noreply.github.com>
2026-07-12 22:01:52 +0300

    upd

--- vps/wg_install.sh
+++ vps/wg_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 IFACE=$(ip route show default | awk '/default/ {print $5; exit}')
-cp "$1" /etc/wireguard/wg0.conf
 echo "net.ipv4.ip_forward = 1" > /etc/sysctl.d/99-wg-forward.conf
 sysctl -p /etc/sysctl.d/99-wg-forward.conf
 echo "iptables-persistent iptables-persistent/autosave_v4 boolean true" | debconf-set-selections
 echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" | debconf-set-selections
 apt-get update && apt-get install -y wireguard iptables-persistent
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -t nat -A POSTROUTING -o "$IFACE" -j MASQUERADE
 iptables -A FORWARD -i wg0 -j ACCEPT
 iptables -A FORWARD -o wg0 -j ACCEPT
